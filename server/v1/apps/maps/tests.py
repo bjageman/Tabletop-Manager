175ff4ccb0a436150b90b5d91617b7d5045caa13
Braden Jageman <bjageman@gmail.com>
2017-07-24 19:43:25 -0700

    More test files cleanup

--- server/v1/apps/maps/tests.py
+++ server/v1/apps/maps/tests.py
@@ -1,22 +1,22 @@
-from . import CampaignTests
+from testing.requests import CampaignTests
 import json
 from testing.utils import bytes_to_json
 
-class WikiTests(CampaignTests):
-    event_name = "Test Wiki"
+class MapTests(CampaignTests):
+    event_name = "Test Map"
     author_id = 1
     size = 5
 
     def setUp(self):
         super().setUp()
-        self.url = self.url + "/"+ str(self.campaign_id) + "/wiki"
+        self.url = self.url + "/"+ str(self.campaign_id) + "/maps"
         for i in range(self.size):
-            rv = self.createWiki(self.url, self.event_name, self.author_id)
+            rv = self.createMap(self.url, self.event_name, self.author_id)
         result = bytes_to_json(rv.data)
         print(result)
-        self.wiki_id = result['id']
+        self.maps_id = result['id']
 
-    def createWiki(self, url, name, author_id):
+    def createMap(self, url, name, author_id):
         return self.app.post(url,
             data=json.dumps({
                 "name": name,
@@ -25,12 +25,12 @@ class WikiTests(CampaignTests):
             content_type='application/json'
         )
 
-    def test_get_wiki(self):
+    def test_get_maps(self):
         rv = self.app.get(self.url)
         result = bytes_to_json(rv.data)
         print(rv, result)
         assert len(result) == self.size
-        rv = self.app.get(self.url + "/" + str(self.wiki_id))
+        rv = self.app.get(self.url + "/" + str(self.maps_id))
         result = bytes_to_json(rv.data)
-        assert result['id'] == self.wiki_id
+        assert result['id'] == self.maps_id
         assert result['campaign_id'] == self.campaign_id
--- server/v1/apps/wiki/tests.py
+++ server/v1/apps/wiki/tests.py
@@ -1,21 +1,22 @@
-from . import CampaignTests
+from testing.requests import CampaignTests
 import json
 from testing.utils import bytes_to_json
 
-class CharacterTests(CampaignTests):
-    character_name = "Test Broski"
+class WikiTests(CampaignTests):
+    event_name = "Test Wiki"
     author_id = 1
     size = 5
 
     def setUp(self):
         super().setUp()
-        self.url = self.url + "/"+ str(self.campaign_id) + "/characters"
+        self.url = self.url + "/"+ str(self.campaign_id) + "/wiki"
         for i in range(self.size):
-            rv = self.createCharacter(self.url, self.character_name, self.author_id)
+            rv = self.createWiki(self.url, self.event_name, self.author_id)
         result = bytes_to_json(rv.data)
-        self.character_id = result['id']
+        print(result)
+        self.wiki_id = result['id']
 
-    def createCharacter(self, url, name, author_id):
+    def createWiki(self, url, name, author_id):
         return self.app.post(url,
             data=json.dumps({
                 "name": name,
@@ -24,11 +25,12 @@ class CharacterTests(CampaignTests):
             content_type='application/json'
         )
 
-    def test_get_characters(self):
+    def test_get_wiki(self):
         rv = self.app.get(self.url)
         result = bytes_to_json(rv.data)
+        print(rv, result)
         assert len(result) == self.size
-        rv = self.app.get(self.url + "/" + str(self.character_id))
+        rv = self.app.get(self.url + "/" + str(self.wiki_id))
         result = bytes_to_json(rv.data)
-        assert result['id'] == self.character_id
+        assert result['id'] == self.wiki_id
         assert result['campaign_id'] == self.campaign_id
