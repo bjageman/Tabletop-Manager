107a6a1b3f45192ff202b455e552609ac911f142
Braden Jageman <bjageman@gmail.com>
2017-08-30 16:04:37 -0700

    server fix

--- server/run.py
+++ server/run.py
@@ -1,18 +1,8 @@
+import os
 from flask_script import Manager, Server
 
 from v1.apps import app, socketio
 
-# manager = Manager(app)
-#
-# manager.add_command("runserver", Server(
-#     use_debugger = True,
-#     use_reloader = True,
-#     host = '0.0.0.0',
-#     port = 5000)
-# )
-
-# if __name__ == "__main__":
-#     manager.run()
-
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, debug=True, port=port, host='0.0.0.0')
