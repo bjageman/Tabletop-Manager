1f3016fe3d6b634d5aa0c752a1fe10ae4152cc7a
Braden Jageman <bjageman@gmail.com>
2017-09-05 17:19:03 -0700

    postgres

--- server/v1/apps/config.py
+++ server/v1/apps/config.py
@@ -4,11 +4,12 @@ DB_USER = (os.environ.get('DB_USER','root'))
 DB_PASS = (os.environ.get('DB_PASS','password'))
 DB_HOST = (os.environ.get('DB_HOST','host'))
 DB_DATABASE = (os.environ.get('DB_DATABASE','database'))
-DB_TEST = (os.environ.get('DB_TEST','test'))
 DB_TYPE = (os.environ.get('DB_TYPE','mysql'))
 DB_PORT = (os.environ.get('DB_PORT','3306'))
 
+DB_TEST = (os.environ.get('DB_TEST','test'))
 DATABASE = DB_TYPE + '://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + ":" + DB_PORT + '/' + DB_DATABASE
+
 DATABASE_TEST = DB_TYPE + 'mysql://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + ":" + DB_PORT + '/' + DB_TEST
 
 DATETIMEFORMAT = "%m/%d/%Y %I:%M %p"
